35d6adc455b74ab75ee4719b66b7523860df58cd
Adhvaith prasad <78650458+Waffle360@users.noreply.github.com>
2021-03-26 10:10:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -41,3 +41,67 @@ class SerialLEDController {
         }
     }
 }
+const serialLEDController = new SerialLEDController();
+const serialMessagesContainer = document.getElementById('serial-messages-container');
+var url = null
+const editor = ace.edit('editor')
+document.getElementById('editor').style.fontSize='16px';
+editor.setTheme("ace/theme/monokai")
+editor.session.setMode("ace/mode/html");
+ editor.setShowPrintMargin(false);
+editor.renderer.setShowGutter(true);
+editor.session.setUseWorker(false)
+function createUrl(html) {
+  var blob = new Blob([html], { type: 'text/html' })
+  return URL.createObjectURL(blob)
+}
+function removeUrl(url) {
+  URL.revokeObjectURL(url)
+}
+function getEditorCode() {
+  return editor.getValue()
+}
+
+if(editor.find('on')){
+serialLEDController.write('1');
+}
+if(editor.find('off')){
+serialLEDController.write('0');
+}
+if(editor.find('getSerialMessage()')){
+console.log(getSerialMessage());
+} 
+function buttonclick() {
+serialLEDController.init();
+
+}
+async function getSerialMessage() {
+      serialMessagesContainer.innerText += await serialLEDController.read() + '\n';
+}
+// Get the modal
+function buttonclick68() {
+  var code = getEditorCode()
+  removeUrl(url)
+  url = createUrl(code)
+  window.open(url);}
+function download_txt() {
+ var save = prompt("Save file as", "Mylaunch");
+  var textToSave = editor.getValue() ;
+  var hiddenElement = document.createElement('a');
+  hiddenElement.href = 'data:attachment/text,' + encodeURI(textToSave);
+  hiddenElement.target = '_blank';
+  hiddenElement.download = save + ".txt";
+  hiddenElement.click();
+}
+document.getElementById('test').addEventListener('click', download_txt);
+document.getElementById('file-upload') 
+            .addEventListener('change', function() { 
+              
+            var fr=new FileReader(); 
+            fr.onload=function(){ 
+                document.getElementById('output') 
+                editor.setValue(fr.result); 
+            } 
+              
+            fr.readAsText(this.files[0]); 
+        }) 
